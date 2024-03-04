6d46d93cc0a9e3715aac7a1808901c7728798f23
Rafidul01 <rafidulhasan555@gmail.com>
2024-03-04 09:14:07 +0600

    made mark as read and tile sction dynamic

--- js/retro.js
+++ js/retro.js
@@ -2,6 +2,7 @@ console.log("hello ami aya porchii!");
 const postContainer = document.getElementById("post-container");
 // faching data
 const allPosts = async (inputValue) => {
+    postContainer.innerHTML=``;
     const result = await fetch(`https://openapi.programming-hero.com/api/retro-forum/posts?category=${inputValue}`);
     const data = await result.json();
     const posts = data.posts;
@@ -14,7 +15,10 @@ const allPosts = async (inputValue) => {
 } 
 allPosts('');
 const appendPosts = (post) => {
-    console.log(post);
+    // console.log(post);
+    // const titleId = post.id +"Title";
+    // const viewId = post.id +"View";
+    // console.log(titleId, viewId);
     let color = "#FF3434";
     if(post.isActive){
         color = "#10B981";
@@ -34,16 +38,16 @@ const appendPosts = (post) => {
             <p># ${post.category}</p>
             <p>Author : ${post.author.name}</p>
         </div>
-        <h1 class="mulish font-bold text-[20px] mb-[16px] text-[#12132D]">${post.title}</h1>
+        <h1 id="${post.id}Title" class="mulish font-bold text-[20px] mb-[16px] text-[#12132D]">${post.title}</h1>
         <p class="inter text-base font-normal mb-[20px] text-[#12132D99] lg:w-[569px]">${post.description}</p>
         <hr class="border border-dashed mb-[20px]">
         <div class="flex items-center justify-between text-[#12132D99] inter font-normal">
             <div class="flex gap-[25px]">
                 <p><i class="fa-regular fa-message"></i> ${post.comment_count}</p>
-                <p><i class="fa-regular fa-eye"></i> ${post.view_count}</p>
+                <p id="${post.id}View"><i class="fa-regular fa-eye"></i> ${post.view_count}</p>
                 <p><i class="fa-regular fa-clock"></i>${post.posted_time}</p>
             </div>
-            <button class="btn w-[50px] h-[50px]  text-[28px] bg-green-500 rounded-full text-white"><i class="fa-solid fa-envelope-open"></i></button>
+            <button  id="${post.id}" onclick="titleView(${post.id})" class="btn w-[50px] h-[50px]  text-[28px] bg-green-500 rounded-full text-white"><i class="fa-solid fa-envelope-open"></i></button>
         </div>
     </div>
     `;
@@ -56,5 +60,24 @@ const searchByCategory = () => {
     const inputFild = document.getElementById("search-value");
     const inputValue = inputFild.value;
     inputFild.value = '';
+    allPosts(inputValue);
     console.log(inputValue);
+}
+const titleContainer = document.getElementById("title-container");
+const titleView = (id) => {
+    console.log(id);
+    const titleCount = document.getElementById("title-count");
+    const titleCountValue = parseInt(titleCount.innerText);
+    titleCount.innerText=titleCountValue+1;
+    const titleId = id +"Title";
+    const viewId = id+"View";
+    console.log(document.getElementById(titleId).innerText);
+    const titleCards = document.createElement('div');
+    titleCards.classList=`p-[16px] flex bg-white rounded-[16px]`;
+    titleCards.innerHTML=`
+    <h1 class="mulish font-semibold text-base  text-[#12132D]">${document.getElementById(titleId).innerText}</h1>
+    <p class="text-[#12132D99] inter font-normal flex items-center gap-2"><i class="fa-regular fa-eye "></i> ${document.getElementById(viewId).innerText}</p>
+    `;
+    titleContainer.appendChild(titleCards);
+
 }
